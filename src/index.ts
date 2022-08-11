89b4f8315027973452cec18318fcdb183304049f
Raman <vincapto@gmail.com>
2022-08-11 05:18:14 +0200

    refactor not done

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,37 @@
 export {};
 import './styles/index.scss';
 import './sas';
-// import './keyboard';
-import { createKeyboardNode, createKeyNode, createContainer } from './keyboard';
 import { Virtual } from './virtual';
 
-const keyboard = createKeyboardNode();
+const activeFunctionList = ['CapsLock', 'Shift', 'Alt', 'Ctrl'];
 const virtual = new Virtual();
-// const nodesVirtual = virtual.getNodes(clickListener);
 const textArea: any = document.querySelector('.textArea');
-// keyboard.append(...nodesVirtual);
-// document.querySelector('.keyboardContainer').appendChild(keyboard);
 
-document.querySelector('.template').append(...virtual.createKeyboard(''));
+document.querySelector('.template').append(...virtual.createKeyboard(clickListener));
 
-function appendElement(containerName: any, element: any) {
-  const container = createContainer(containerName);
-  container.append(...element);
-  return container;
-}
-updateKeyboard();
-
-function funcClick(element: any) {
-  // virtual.caretPosition = getCaret();
+function updateFunctionList(element: any) {
   if (!virtual.funcList.includes(element.dataset.code)) {
     virtual.pushFunc(element.dataset.code);
   } else {
     const index = virtual.funcList.indexOf(element.dataset.code);
     virtual.funcList.splice(index, 1);
-    virtual.nodes[element.dataset.code].classList.toggle('active');
-  }
-  console.log('FUNCTIONS', virtual.funcList);
-  //@ts-ignore
-  virtual.funcEvents[element.dataset.name]();
-  // console.log('UPPER', virtual.isUpper);
-  // element.target.classList.toggle('active');
-  // virtual.funcList.map((a) => {
-  //   console.log('FUNCTRION++++++++++++++++++++++++++++++++++', a);
-  //   if (virtual.funcList.length != 0) if (virtual.nodes[a]) virtual.nodes[a].classList.add('active');
-  // });
-  // if (this.isShift)
-
-  virtual.addActive();
-  // animateClick(element);
-  if (virtual.isChanged) updateKeyboard();
-  if (virtual.checkLang()) {
-    updateKeyboard();
+    virtual.keyboardNodes[element.dataset.code].classList.toggle('active');
   }
 }
 
+function funcClick(element: any) {
+  if (activeFunctionList.includes(element.dataset.name)) updateFunctionList(element);
+  //@ts-ignore
+  virtual.funcEvents[element.dataset.name]();
+  virtual.checkActiveButton();
+  virtual.checkLang();
+  if (virtual.isChanged) updateKeyboard();
+}
+
 function letterClick(element: any) {
-  // virtual.caretPosition = getCaret();
-  console.log('FUNCTIONS', virtual.funcList);
-  // console.log('isShift', virtual.isShift);
-  // virtual.textArea += element.target.dataset.name;
-  // virtual.textArea = 'dasdads';
   virtual.addLetter(element.dataset.name);
-  // console.log('POSITION+++++++++++++', virtual.caretPosition);
   textArea.value = virtual.textArea;
-  // virtual.clearFuncList();
+
   if (!virtual.funcList.includes('CapsLock')) {
     virtual.clearFuncList();
   } else {
@@ -66,11 +39,8 @@ function letterClick(element: any) {
     virtual.pushFunc('CapsLock');
   }
   if (virtual.isShift) {
-    virtual.toggleShift();
-    virtual.toggleUpper();
-    virtual.isChanged = true;
+    virtual.pressShift();
   }
-  console.log('ISCHANGES', virtual.isChanged);
   if (virtual.isChanged) updateKeyboard();
   textArea.value = virtual.textArea;
   virtual.isShift = false;
@@ -79,8 +49,6 @@ function letterClick(element: any) {
 
 function catchClick(element: any) {
   const type = element.dataset.type;
-  console.log('=============================');
-  console.log(element);
   if (type === 'func') {
     funcClick(element);
   }
@@ -91,118 +59,39 @@ function catchClick(element: any) {
 }
 
 function clickListener(element: any) {
-  // animateClick(element);
-  console.log('=============================');
-  console.log(element);
   catchClick(element.target);
 }
 
 function animateClick(element: any) {
-  console.log('ANIMATION', element);
   element.animate([{ transform: 'translate3D(0, 0, 0)' }, { transform: 'translate3D(0, 3px, 0)' }], 100);
 }
 
 function updateKeyboard() {
-  // keyboard.append(...virtual.getNodes(clickListener, active));
-  // let letterKeys = document.querySelector('.letters');
-  // const numsKeys = document.querySelector('.nums');
-
-  // numsKeys.innerHTML = '';
-  // const nums = virtual.getNumKeys();
-  // const alphabet = virtual.getAlphabetKeys();
-  // const lettersNodes = virtual.updateKeys(alphabet, clickListener);
-  // const keysNodes = virtual.updateNumKeys(nums, clickListener);
-  // virtual.lettersNodes = lettersNodes;
-
-  // const container = createContainer(`container-keys letters`);
-
-  // container.append(...lettersNodes);
-  // letterKeys.outerHTML = container.outerHTML;
-
-  // rewriteNodes('.letters');
-  // rewriteNodesByClass();
-  rewriteNodesByClassKeyboard('letter');
-  rewriteNodesByClassKeyboardNum('.num');
-  // rewriteNodesByClassKeyboard('.num');
-  // numsKeys.append(...keysNodes);
+  rewriteNodes();
   textArea.value = virtual.textArea;
   virtual.isChanged = false;
 }
 
-function rewriteNodesByClassKeyboardNum(className = '.num') {
-  const nodes = document.querySelectorAll(className);
-  console.log('NODES TEMPLATE', virtual.lettersNodes);
-  const nums = virtual.getNumKeys();
-  const lettersNodes = virtual.updateNumKeys(nums, 'num', clickListener);
-
-  // console.log(' VIRTUAL NODE', virtual.lettersNodes[65]);
-  // console.log('LETTER NODE', lettersNodes[0]);
-  nodes.forEach((element: any, key: number) => {
-    const newElement = virtual.lettersNodes[element.dataset.code];
-    // element.addEventListener('click', () => alert(''));
-    element.parentNode.replaceChild(newElement, element);
-    // console.log('3====', element);
-  });
+function rewriteNodes() {
+  const nodes = virtual.checkNodes(clickListener);
+  if (nodes.length !== 0) {
+    nodes.map((a) => {
+      document.querySelectorAll('.' + a).forEach((element: any) => {
+        const newElement = virtual.keyboardNodes[element.dataset.code];
+        element.parentNode.replaceChild(newElement, element);
+      });
+    });
+  }
 }
-
-function rewriteNodesByClassKeyboard(className = 'letter') {
-  const nodes = document.querySelectorAll('.' + className);
-  // console.log('NODES TEMPLATE', virtual.lettersNodes);
-  const alphabet = virtual.getAlphabetKeys();
-  console.log(alphabet);
-  const lettersNodes = virtual.updateKeys(alphabet, className, clickListener);
-
-  // console.log(' VIRTUAL NODE', virtual.lettersNodes[65]);
-  // console.log('LETTER NODE', lettersNodes[0]);
-  nodes.forEach((element: any, key: number) => {
-    const newElement = virtual.lettersNodes[element.dataset.code];
-    // element.addEventListener('click', () => alert(''));
-    element.parentNode.replaceChild(newElement, element);
-    // console.log('3====', element);
-  });
-}
-// function rewriteNodesByClass(className = '.change') {
-//   const nodes = document.querySelectorAll(className);
-
-//   const alphabet = virtual.getAlphabetKeys();
-//   const lettersNodes = virtual.updateKeys(alphabet, clickListener);
-
-//   // console.log(' VIRTUAL NODE', virtual.lettersNodes[65]);
-//   // console.log('LETTER NODE', lettersNodes[0]);
-//   nodes.forEach((element: any, key: number) => {
-//     const newElement = virtual.lettersNodes[element.dataset.code];
-//     element.parentNode.replaceChild(newElement, element);
-//     // console.log('3====', element);
-//   });
-// }
-function rewriteNodes(name: any) {
-  const nodes = document.querySelector(name).childNodes;
-  nodes.forEach((element: any) => {
-    element.addEventListener('click', clickListener);
-    //@ts-ignore
-    virtual.lettersNodes[element.dataset.code] = element;
-  });
-}
-
-// document.addEventListener('keypress', (event) => {
-//   const keyName = event.key;
-//   alert('asd' + event.keyCode);
-//   catchClick(virtual.lettersNodes[event.which]);
-//   // alert('keypress event\n\n' + 'key: ' + keyName);
-// });
 
 document.addEventListener(
   'keydown',
   (event) => {
     event.preventDefault;
     event.stopPropagation;
-    if (event.repeat == false) {
-      const type = virtual.nodes[event.which].dataset.type;
-      if (type !== 'func') catchClick(virtual.lettersNodes[event.which]);
-      else catchClick(virtual.nodes[event.which]);
+    if (event.repeat == false && virtual.keyboardNodes[event.which]) {
+      catchClick(virtual.keyboardNodes[event.which]);
     }
-    // document.querySelector('body').classList.add('actvie');
-    // console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\', virtual.lettersNodes[event.which]);
   },
   false
 );
@@ -214,21 +103,3 @@ function getCaret() {
 textArea.addEventListener('click', () => {
   virtual.caretPosition = getCaret();
 });
-
-// function createNodes(elements: string[], className: string) {
-//   const arr: HTMLElement[] = elements.map((a) => createKeyNode(a, className));
-//   arr.forEach((element) => {
-//     element.addEventListener('click', clickListener);
-//   });
-//   return arr;
-// }
-
-// const aContainer = createContainer('alphabet-container');
-// const alphabetNodes = appendElement('alphabet-container', createNodes(alphabet, 'key'));
-// const numsNodes = appendElement('nums-container', createNodes(nums, 'key'));
-// const funcNodes = appendElement('functional-container', createNodes(functional, 'key'));
-// aContainer.append(...alphabetArr);
-// keyboard.append(numsNodes);
-// keyboard.append(alphabetNodes);
-// keyboard.append(funcNodes);
-// console.log(b);
--- src/keyboard.ts
+++ src/keyboard.ts
@@ -15,11 +15,35 @@ function createContainer(panelName: string) {
   return div;
 }
 
-function createKeyNode(text: string, className: string) {
+const keyboardTamplate = {
+  1: [
+    { arr: '1 2 3 4 5 6 7 8 9 0 - =', type: 'num' },
+    { arr: 'Backspace', type: 'func' },
+  ],
+  2: [
+    { arr: 'Tab', type: 'func' },
+    { arr: 'q w e r t y u i o p', type: 'letter' },
+    { arr: 'Delete', type: 'func' },
+  ],
+  3: [
+    { arr: 'CapsLock', type: 'func' },
+    { arr: 'a s d f g h j k l', type: 'letter' },
+    { arr: 'Enter', type: 'func' },
+  ],
+  4: [
+    { arr: 'Shift', type: 'func' },
+    { arr: 'z x c v b n m', type: 'letter' },
+    { arr: 'Up Shift', type: 'func' },
+  ],
+  5: [{ arr: 'Ctrl Alt Space Left Down Right', type: 'func' }],
+};
+
+function createKeyNode(text: string, code: string, className: string, type: string) {
   const div = document.createElement('div');
   div.className = className;
   div.innerHTML = text;
-  div.dataset.type = text;
+  div.dataset.type = type;
+  div.dataset.code = code;
   div.dataset.name = text;
   return div;
 }
@@ -28,4 +52,4 @@ function createKey(text: string) {
   <div class='key btn'>${text}</div>
   `;
 }
-export { createKey, createKeyboard, createKeyNode, createKeyboardNode, createContainer };
+export { createKey, createKeyboard, createKeyNode, createKeyboardNode, createContainer, keyboardTamplate };
